8011c31230dc0900ff486a9212ae9c3d77523661
Wooseobee <dntjq0807@naver.com>
2022-10-07 15:58:10 +0900

    Redis 수정

--- src/main/java/capstone3d/Server/config/RedisConfig.java
+++ src/main/java/capstone3d/Server/config/RedisConfig.java
@@ -13,10 +13,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.redis.host")
+    @Value("${spring.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port")
+    @Value("${spring.redis.port}")
     private int port;
 
     @Bean
