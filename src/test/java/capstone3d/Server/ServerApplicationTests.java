8bdbbc94622112d81ecaade43a8fd90283e6fc53
Wooseobee <dntjq0807@naver.com>
2022-10-18 15:48:48 +0900

    contextloads() failed 에러 해결

--- src/test/java/capstone3d/Server/ServerApplicationTests.java
+++ src/test/java/capstone3d/Server/ServerApplicationTests.java
@@ -3,7 +3,7 @@ package capstone3d.Server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ServerApplicationTests {
 
 	@Test
